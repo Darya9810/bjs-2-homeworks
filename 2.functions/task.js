dbc3502652351693d164c77dc7db377545a3b239
Vladimir Chebukin <vovachebr@mail.ru>
2022-12-23 10:33:59 +0300

    mistake in async

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,45 @@
-function getArrayParams(...arr) {
+function getArrayParams(arr) {
+  min = arr[0];
+  max = arr[0];
+  sum = arr[0];
 
-  return { min: min, max: max, avg: avg };
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum +=  arr[i]
+  }
+  let avg = sum / arr.length;
+
+  return { min: min, max: max, avg: +avg.toFixed(2)};
 }
 
-function summElementsWorker(...arr) {
 
+// Задание 2
+function worker(arr) {
+  let summa = (sum, elem) => sum + elem;
+  return arr.reduce(summa);
 }
 
-function differenceMaxMinWorker(...arr) {
+function makeWork(arrOfArr, func) {
+  let max = func(arrOfArr[0]);
 
+  for (let i = 1; i < arrOfArr.length; i++) {
+    const funcResult = func(arrOfArr[i]);
+    if (funcResult > max) {
+      max = funcResult;
+    }
+  }
+  return max;
 }
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+// Задание 3
+function worker2(arr) {
+  let max = Math.max.apply(null, arr);
+  let min = Math.min.apply(null, arr);
+  return max - min;
 }
