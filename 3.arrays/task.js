75e94c0c9f2d463b2c9930190e802f3088182d24
Чей-то ПК <Dasha_muraveva98@list.ru>
2023-01-23 23:07:06 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,16 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-    let result = true;
-    if (arr1.length != arr2.length) {
-        result = false;
-    }
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] != arr2[i]) {
-            result = false;
-        }
-    }
+
+    let result = arr1.length === arr2.length &&
+        arr1.every((value, index) => value === arr2[index]);
     return result;
+
 }
 
 function advancedFilter(arr) {
-    let resultArr = arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item *= 10);
+
+    let resultArr = arr.filter(e => e > 0 && e % 3 === 0).map((item) => item * 10);
+
     return resultArr;
 }
