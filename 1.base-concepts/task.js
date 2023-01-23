b38a731eb858ea448326bf2a4c72da3871476ec7
Чей-то ПК <Dasha_muraveva98@list.ru>
2023-01-23 23:02:25 +0300

    base

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,34 +1,66 @@
 "use strict";
 
+"use strict";
+
+// Задача 1.1
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let disc = b * b - 4 * a * c;
-  if (disc > 0) {
-    arr.push((-b + Math.sqrt(disc)) / 2 / a);
-    arr.push((-b - Math.sqrt(disc)) / 2 / a);
-  } else if (disc == 0) {
-    arr.push(-b / 2 / a);
+  let arr;
+
+  // код для задачи №1 писать здесь
+
+  arr = [];
+  let d = (b**2-4*a*c);
+  if (d > 0){
+    arr.push((-b + Math.sqrt(d) )/(2*a));
+    arr.push((-b - Math.sqrt(d) )/(2*a));
   }
-  return arr;
+  if (d === 0){
+    arr.push(-b/(2*a));
+  }
+  return arr; // array
 }
 
+
+// Задача 1.2
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  percent = parseFloat(percent);
-  contribution = parseInt(contribution);
-  amount = parseInt(amount);
+  let totalAmount;
 
-  //todo added validate
+  // код для задачи №2 писать здесь
 
-  let endDate = new Date(date);
-  let nowDate = new Date(Date.now);
+  // Блок валидации число/не число поля "Процентная ставка"
+  if (Number.isFinite(percent)){
+    percent = Number(percent)
+  } else {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
 
-  //todo fix NaN
-  let totalMonth = (endDate.getFullYear - nowDate.getFullYear) * 12 + endDate.getMonth - nowDate.getMonth;
+  // Блок валидации число/нечисло поля "Начальный взнос"
+  if (Number.isFinite(contribution)){
+    contribution = Number(contribution)
+  } else {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
 
-  let percentPerMonth = percent / 12;
+  // Блок валидации число/нечисло поля "Общая стоимость"
+  if (Number.isFinite(amount)){
+    amount = Number(amount)
+  } else {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
 
-  let amountPerMonth = amount * (percentPerMonth + (percentPerMonth / (Math.pow(1 + percentPerMonth, totalMonth))));
-  let totalAmount = (amountPerMonth * totalMonth).toFixed(2);
-  console.log(totalAmount);
-  return totalAmount;
+  let creditBody = (amount - contribution);
+  let dateStart = new Date();
+
+  let monthsOfCredit = (
+      date.getMonth() - dateStart.getMonth() + (12 * (date.getFullYear() - dateStart.getFullYear()))
+  );
+  let monthPayment = (creditBody * (percent/1200 + ((percent/1200) / (((1 + (percent/1200)) ** monthsOfCredit) - 1))));
+
+  totalAmount = monthPayment * monthsOfCredit;
+
+  //console.log(Number(totalAmount.toFixed(2)));
+
+  return Number(totalAmount.toFixed(2));
 }
